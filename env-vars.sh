26144964da4d309f7a3d16c1c0af04776474ac2e
Elifarley C <elifarley@users.noreply.github.com>
2016-05-03 20:49:27 -0300

    Create env-vars.sh

---
+++ env-vars.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+ENV_VARS_DIR="${ENV_VARS_DIR:-/mnt-env-vars}"
+PHUSION_ENV_DIR=/etc/container_environment
+
+test -d "$ENV_VARS_DIR" && test "$(ls -A "$ENV_VARS_DIR")" || exit 0
+
+test -d "$PHUSION_ENV_DIR" && {
+  cp -av "$ENV_VARS_DIR"/* "$PHUSION_ENV_DIR" && \
+  exit 0
+}
+
+for v in "$ENV_VARS_DIR"/* do;
+  export "$(basename "$v")"="$(cat "$v")"
+done
