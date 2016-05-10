8904ca534db2f59e37567df2d01bca845eb8407f
Elifarley C <elifarley@users.noreply.github.com>
2016-05-10 19:51:54 -0300

    "exit" -> "return" to avoid exiting the calling script (entry.sh)

--- env-vars.sh
+++ env-vars.sh
@@ -4,11 +4,11 @@ set -e
 ENV_VARS_DIR="${ENV_VARS_DIR:-/mnt-env-vars}"
 PHUSION_ENV_DIR=/etc/container_environment
 
-test -d "$ENV_VARS_DIR" && test "$(ls -A "$ENV_VARS_DIR")" || exit 0
+test -d "$ENV_VARS_DIR" && test "$(ls -A "$ENV_VARS_DIR")" || return 0
 
 test -d "$PHUSION_ENV_DIR" && {
   cp -av "$ENV_VARS_DIR"/* "$PHUSION_ENV_DIR" && \
-  exit 0
+  return 0
 }
 
 for v in "$ENV_VARS_DIR"/* do;
