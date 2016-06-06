6f95d691d2a49797665f404163821574432905a2
Elifarley C <elifarley@users.noreply.github.com>
2016-06-06 19:37:20 -0300

    More status messages

--- entry.sh
+++ entry.sh
@@ -4,7 +4,7 @@ set -e
 
 test "$DEBUG" && set -x
 
-test -r "$CMD_BASE"/env-vars.sh && \
+test -r "$CMD_BASE"/env-vars.sh && echo "Loading '$CMD_BASE/env-vars.sh'..." && \
   . "$CMD_BASE"/env-vars.sh
 
 DAEMON="${DAEMON:-sshd}"
@@ -15,7 +15,8 @@ dir_not_empty() { test "$(ls -A "$@" 2>/dev/null)" ;}
 
 # Copy default config from cache
 dir_not_empty /etc/ssh || {
-  dir_not_empty /etc/ssh.cache && cp -a /etc/ssh.cache/* /etc/ssh/
+  echo "/etc/ssh is empty."
+  dir_not_empty /etc/ssh.cache && cp -av /etc/ssh.cache/* /etc/ssh/
 }
 
 # Generate Host keys, if required
@@ -26,12 +27,12 @@ dir_not_empty /etc/ssh/ssh_host_* || {
 test -d "$HOME"/.ssh || mkdir "$HOME"/.ssh
 # Fix permissions, if writable
 test ! -w "$HOME"/.ssh && echo "WARNING: '$HOME/.ssh' is not writeable" || {
-  dir_not_empty "$SSH_CONFIG_VOLUME" && cp -a "$SSH_CONFIG_VOLUME"/* "$HOME"/.ssh
+  dir_not_empty "$SSH_CONFIG_VOLUME" && cp -av "$SSH_CONFIG_VOLUME"/* "$HOME"/.ssh
   chown -R $_USER:$_USER "$HOME"/.ssh && chmod -R 600 "$HOME"/.ssh && chmod 700 "$HOME"/.ssh
 }
 
 test -r "$HOME"/.ssh/docker-config.json && \
-  mkdir -p "$HOME"/.docker && ln -s ../.ssh/docker-config.json $HOME/.docker/config.json
+  mkdir -p "$HOME"/.docker && ln -sv ../.ssh/docker-config.json $HOME/.docker/config.json
 
 ak="$HOME"/.ssh/authorized_keys
 test ! -f "$ak" && echo "WARNING: No SSH authorized_keys found at '$ak'" || {
@@ -44,12 +45,15 @@ echo "[$_USER] Running $@"
 test -S /var/run/docker.sock && test $(id -u $_USER) != $(stat -c "%u" /var/run/docker.sock) && {
   docker_group=$(stat -c "%g" /var/run/docker.sock)
   getent group "$docker_group" || groupadd -g "$docker_group" docker
+  echo "Changing GID to '$docker_group' to match '/var/run/docker.sock'..."
   usermod -g "$docker_group" $_USER
 }
 
 if test -e "$MNT_DIR"; then
+  echo "Changing UID to match '$MNT_DIR'..."
   usermod -u $(stat -c "%u" "$MNT_DIR") $_USER
 else
+  echo "Creating '$MNT_DIR'..."
   mkdir "$MNT_DIR" && chown $_USER:$_USER "$MNT_DIR"
 fi
 
@@ -62,18 +66,7 @@ if test root != "$_USER"; then
     mkdir -p /var/run/sshd && chmod 0755 /var/run/sshd
 fi
 
-if [ "$(basename $1)" == "$DAEMON" ]; then
-    if test $(id -un) != "$_USER"; then
-      echo "gosu $_USER $@"
-      exec gosu $_USER $@
-    else
-      exec $@
-    fi
-    exit $?
-fi
+test "$(id -un)" = "$_USER" && echo exec "$@" && exec "$@"
 
-if test $(id -un) != "$_USER"; then
-  exec gosu $_USER "$@"
-else
-  exec "$@"
-fi
+echo exec gosu $_USER "$@"
+exec gosu $_USER "$@"
