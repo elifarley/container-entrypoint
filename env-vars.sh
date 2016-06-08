e499be6d34b166384eb06df9cc31d3ec8fc2ff1f
Elifarley C <elifarley@users.noreply.github.com>
2016-06-08 19:14:21 -0300

    Copy env vars to ~/.ssh/environment

--- env-vars.sh
+++ env-vars.sh
@@ -9,14 +9,26 @@ test -d "$ENV_VARS_DIR" && test "$(ls -A "$ENV_VARS_DIR")" || {
   return
 }
 
+process_env_file() {
+  local env_file="$1"
+  local to_export="$2"
+
+  local name="$(basename "$env_file")"; local val="$(cat "$env_file")"
+  echo "$name='$val'"
+  test "$to_export" && export "$name"="$val"
+  printf "%s='%s'\n" "$name" "$val" >> "$HOME"/.ssh/environment
+}
+
 test -d "$PHUSION_ENV_DIR" && {
   echo "Copying env var files from '$ENV_VARS_DIR' to '$PHUSION_ENV_DIR'..."
   cp -av "$ENV_VARS_DIR"/* "$PHUSION_ENV_DIR"
+  for v in "$ENV_VARS_DIR"/*; do
+    process_env_file "$v"
+  done
   return
 }
 
 echo "Loading env var files from '$ENV_VARS_DIR'..."
 for v in "$ENV_VARS_DIR"/*; do
-  echo "$(basename "$v")"="$(cat "$v")"
-  export "$(basename "$v")"="$(cat "$v")"
+  process_env_file "$v" true
 done
