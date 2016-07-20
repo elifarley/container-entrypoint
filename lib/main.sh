0376400832336a408a3f0b4396243c80112695a9
Elifarley C <elifarley@users.noreply.github.com>
2016-07-20 18:39:37 -0300

    linklogfile: new function. Fixes #2

--- lib/main.sh
+++ lib/main.sh
@@ -1,4 +1,15 @@
 dir_not_empty() { test "$(ls -A "$@" 2>/dev/null)" ;}
+foreach() { local cmd="$1"; shift; for i; do $cmd "$i"; done ;}
+argsep() { local IFS="$1"; shift; local cmd="$1"; shift; set -- $@; $cmd "$@" ;}
+
+linklogfiles() { foreach linklogfile "$@" ;}
+linklogfile() {
+  local logfile="$1";
+  local target="${logfile##*:}"; test "$target" = "$logfile" && target=''
+  test "$target" && logfile="${logfile%%:*}" && case "$target" in out) target=1;; err) target=2;; esac
+  test -L "$logfile" -o -e "$logfile" && ls -Falk "$logfile" && return
+  ln -vsf /proc/self/fd/"$target" "$logfile"
+}
 
 psudo() {
   local sudo=''; type 1>/dev/null su-exec && sudo=su-exec
