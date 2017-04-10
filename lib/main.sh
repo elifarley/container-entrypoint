8e12f6723704780c1d588a08a760ce58d9ad1c1e
Elifarley C <elifarley@users.noreply.github.com>
2017-04-10 15:35:28 -0300

    New functions: "find_lib_dir" and "ldd_fix"

--- lib/main.sh
+++ lib/main.sh
@@ -2,6 +2,28 @@ dir_not_empty() { test "$(ls -A "$@" 2>/dev/null)" ;}
 foreach() { local cmd="$1"; shift; for i; do $cmd "$i"; done ;}
 argsep() { local IFS="$1"; shift; local cmd="$1"; shift; set -- $@; $cmd "$@" ;}
 
+find_lib_dir() { for m in /lib/x86_64-linux-gnu /lib64; do test -e "$m" && echo $m && break; done ;}
+
+# command lib_repo
+ldd_fix() {
+  local bin="$1" lib_repo="${2:-/mnt-lib-repo}"
+  local main_lib_dir="$(find_lib_dir)" loops=0
+  test -d "$main_lib_dir" || { echo "Main lib dir missing: '$main_lib_dir'"; return 1 ;}
+
+  while true; do
+    ldd $(which "$bin") | egrep '\s*not found' |
+    while IFS= read -r line; do
+      line="$(echo $(echo $line | cut -d= -f1))"
+      echo "'[ldd_fix] $lib_repo/$line' -> '$main_lib_dir'/"
+      ln -s "$lib_repo/$line" "$main_lib_dir"/ || return
+    done
+    loops=$((loops + 1))
+    test $loops -le 400 || return
+    ldd $(which "$bin") | egrep -q '\s*not found' || break
+  done
+
+}
+
 linklogfiles() {
   mkfifo -m 600 /tmp/logpipe_out && mkfifo -m 600 /tmp/logpipe_err && \
   chown "$1" /tmp/logpipe* && shift || return
